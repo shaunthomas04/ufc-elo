4584ee0bf3a62700abc42697936bd5c11e299b35
shaunthomas04 <shaunn.thomas@calbaptist.edu>
2025-12-27 16:36:09 -0800

    saving some minor api channges

--- backend/server/db.py
+++ backend/server/db.py
@@ -1,11 +1,24 @@
 import mysql.connector
+import mysql.connector
+from mysql.connector import errorcode
+import os
+from dotenv import load_dotenv
+
 
 def get_connection():
+    # Load environment variables
+    load_dotenv()
+    DB_USER = os.getenv("DB_USER")
+    DB_PASS = os.getenv("DB_PASS")
+    DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+    DB_NAME = os.getenv("DB_NAME")
+        
+    
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="password",
-        database="ufc_elo"
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASS,
+        database=DB_NAME
     )
 
 def call_procedure(proc_name, args=()):
