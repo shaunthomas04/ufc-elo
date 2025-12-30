ee957e9531d98c2de70f30e867360b2b62df4260
shaunthomas04 <shaunn.thomas@calbaptist.edu>
2025-12-29 22:22:51 -0800

    commit to let the world I am a moron :(,  I will do better

--- backend/server/main.py
+++ backend/server/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from cache import cached
 from db import call_procedure
+from db import get_connection
+from datetime import date
 
 app = FastAPI()
 
@@ -43,6 +45,73 @@ async def get_division_rankings(weight_class: str, year: int, month: int):
 @app.get("/p4p/{year}/{month}")
 async def get_p4p_rankings(year: int, month: int):
     return call_procedure("GetPoundForPoundRankings", (year, month))
+
+
+# Pound-for-pound rankings
+@app.get("/rankings/{year}/{month}/{day}")
+async def get_monthly_rankings(year: int, month: int, day: int):
+
+    WEIGHT_CLASSES = [
+        "Men Flyweight"
+    ]
+
+    ranking_date = date(year, month, day)
+    res = {}
+
+    try:
+        conn = get_connection()
+
+        for weight in WEIGHT_CLASSES:
+            cursor = conn.cursor(dictionary=True)
+
+            cursor.callproc(
+                "GetTopEloRankings",
+                [ranking_date, weight]
+            )
+
+            rows = []
+            for result in cursor.stored_results():
+                rows = result.fetchall()
+
+            res[weight] = rows
+            cursor.close()
+
+        conn.close()
+
+    except Exception as e:
+        print("ERROR:", e)
+        return {"error": str(e)}
+
+    return res
+
+
+
+@app.get("/debug/rankings")
+async def debug_rankings():
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        cursor.callproc(
+            "GetTopEloRankings",
+            [date(2025, 12, 28), "Men Flyweight"]
+        )
+
+        rows = []
+        for result in cursor.stored_results():
+            rows = result.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        return rows
+
+    except Exception as e:
+        print("ERROR:", e)
+        return {"error": str(e)}
+
+
+
 
 
 
